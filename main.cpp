3be819579e5ecb63107988740e6518c989c21285
Jozmon <118296216+Jozmon@users.noreply.github.com>
2023-01-21 11:51:18 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ void menu()
     printf("\n");
     printf("Select one of the following options:\n");
     printf("\n");
-    printf(" 1. Decemical to binary\n");
+    printf(" 1. Decimal to binary\n");
     printf(" 2. Binary to decimal\n");
     printf(" 3. Exit\n");
     
@@ -85,11 +85,11 @@ void bin2dec(){
 
 void dec2bin(){
 	
-	//
-	int binip[4];
+	//ip in binary form
+	//int binip[4];
 	
 
-	//
+	//ip in decimal form
 	int decip[4];
 	
 	//number of conversions
@@ -99,42 +99,41 @@ void dec2bin(){
 	printf("Enter 32-bit IP address in dotted decimal notation (xxx.xxx.xxx.xxx)\n: ");
 	scanf ("%d.%d.%d.%d",&decip[1],&decip[2],&decip[3],&decip[4]);
 	
+	printf ("The decimal ip: %d.%d.%d.%d\n",decip[1],decip[2],decip[3],decip[4]);
+	
 	while ( n < rep ){
 		
 		//check ip validity
 		if ( decip[n] > 255 || decip[n] < 0){
 			printf("Invalid input string (incorrect numbers for IP address)\n");
 		}
-		
-		//conversion variables
-		//int dec=0;
-		//int base=1;
-		//int bin;
-		//int rem;
-		
-		//dec = decip[n];
-		//int tmp = dec;
-		
-		//while ( tmp > 0  ) {
-		
-		//conversion formulas
-		//rem  = tmp % 2;
-		//dec  = dec + rem * base;
-		//tmp  = tmp / 10;
-		//base = base * 2;
-	
-		//}
-	
-		//decip[n] = dec;
+
+		//binip[n] = dec2bin(decip[n]);
+		// Stores binary representation of number.
+		int bin[32]; // Assuming 32 bit integer.
+		int i=0;
+		int dec=decip[n];
+		   
+		while ( dec > 0 ){
+			bin[i++] = dec % 2;
+			dec = dec / 2;
+		}
+		   
+		// Printing array in reverse order.
+		int j = i-1;
+			  
+		while (j >= 0){
+			printf("%d", bin[j]);
+			j--;
+		  }
+		printf (".");
+
 		++n;
-		
 	
 	}
 	
-	printf ("The decimal ip: %d.%d.%d.%d",decip[1],decip[2],decip[3],decip[4]);
-	printf ("The binary ip : %d.%d.%d.%d\n",binip[1],binip[2],binip[3],binip[4]);
 
-
+	//printf ("The binary ip : %d.%d.%d.%d\n",binip[1],binip[2],binip[3],binip[4]);
 	
 	
 
@@ -161,8 +160,7 @@ bin2dec();
 	 
 }else if( mode == 2){
 			
-//bin2dec function	
-printf ("bin2dec function");
+dec2bin();
 	
 }
 		
@@ -176,7 +174,6 @@ printf ("bin2dec function");
   
   
 }
-
 
 
 
